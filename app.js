804d6feb03fd8aa6d8a50aabcf738f422f96e17e
awysocka <wysocka.anna88@gmail.com>
2021-09-10 16:12:59 +0200

    Add endpoints for events data

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+const initialData = require('./initialData');
+
 const app = express();
 const port = 3003;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(cors());
+
+const { events, config, tags } = initialData;
+
+app.get('/events', (req, res) => res.type('json').json(events));
+app.get('/config', (req, res) => res.type('json').json(config));
+app.get('/tags', (req, res) => res.type('json').json(tags));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
