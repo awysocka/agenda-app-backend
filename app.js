745ca9f226e3dce83f9958864cb2d476cd2f1b16
awysocka <wysocka.anna88@gmail.com>
2021-09-20 17:35:30 +0200

    Handle add new event

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
 const initialData = require('./initialData');
 
 const app = express();
@@ -9,8 +10,18 @@ app.use(cors());
 
 const { events, config, tags } = initialData;
 
-app.get('/events', (req, res) => res.type('json').json(events));
-app.get('/config', (req, res) => res.type('json').json(config));
-app.get('/tags', (req, res) => res.type('json').json(tags));
+app.get('/events', (req, res) => res.json(events));
+app.get('/config', (req, res) => res.json(config));
+app.get('/tags', (req, res) => res.json(tags));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/events', (req, res) => {
+  const id = uuidv4();
+  const newEvent = { id, ...req.body };
+  events.push(newEvent);
+  res.json(newEvent);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
